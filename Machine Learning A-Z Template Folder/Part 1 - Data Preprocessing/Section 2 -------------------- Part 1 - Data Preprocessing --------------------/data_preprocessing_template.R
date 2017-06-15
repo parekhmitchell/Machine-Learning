d8a283243ec3100546654ee672c3eced53dd0b9d
Mitchell Parekh <mitchell.parekh@thomsonreuters.com>
2017-06-15 12:27:33 -0400

    Data Preprocessing Completed

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#' Created on Thu Jun 15 10:55:11 2017
+#' 
+#' @author: Mitchell Parekh
+#' Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ﻿# Machine-Learning
 Machine Learning A-Z™: Hands-On Python &amp; R In Data Science
-Testing ... 
